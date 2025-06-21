c3d0f2e86383c6ad2f6b0b63bc3bb9ddcfe82c50
wwh171944 <1641187558@qq.com>
2025-06-21 22:10:58 +0800

    创建后端返回体

---
+++ src/main/java/com/MyCommunity/VO/Response.java
@@ -0,0 +1,34 @@
+package com.MyCommunity.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * 响应体用于后端向前端返回数据
+ * @param <T>
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Response<T> implements Serializable {
+
+    private String code;
+
+    private String msg;
+
+    private T data;
+
+    public static <T> Response<T> buildSuccess(T result) {
+        return new Response<T>("200", "请求成功", result);
+    }
+
+    public static <T> Response<T> buildFailure(String msg, String code) {
+        return new Response<T>(code, msg, null);
+    }
+
+}
